947d3bdca95255f14e1afcc8203be86821f6bb41
Noah Glassford <noahglassford@gmail.com>
2019-11-27 14:57:36 -0500

    Added Sound Preloading function
    I added a sound preloader, this time on the right branch
    LoadSound(std::string Filename);

--- FNAF/FNAF/AudioManager.cpp
+++ FNAF/FNAF/AudioManager.cpp
@@ -16,3 +16,9 @@ void Soundfunctions::PauseSound(std::string Filename)
 	std::string SoundString = "stop assets/Audio_Files/" + Filename;
 	mciSendString(SoundString.c_str(), NULL, 0, NULL);
 }
+
+void Soundfunctions::LoadSound(std::string Filename)
+{
+	std::string SoundString = "open assets/Audio_Files/" + Filename + " type mpegvideo alias mp3";
+	mciSendString(SoundString.c_str(), NULL, 0, NULL);
+}
--- FNAF/FNAF/AudioManager.h
+++ FNAF/FNAF/AudioManager.h
@@ -8,6 +8,7 @@ class Soundfunctions
 {
 public:
 	void PlaySingleSound(std::string Filename);
-	void LoopSound(std::string Filename);
-	void PauseSound(std::string Filename);
+	void LoopSound(std::string Filename); //Loops Sound
+	void PauseSound(std::string Filename); //Stops sound
+	void LoadSound(std::string Filename); //Used to preload sounds during init
 };
